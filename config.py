a1cf2032a1367f72772a8bba949415bf2459e5b1
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-16 01:41:11 +0300

    added instance folder to gitignore

--- config.py
+++ config.py
@@ -16,7 +16,8 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings.
     '''
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://elijah:eliki13720@localhost/blog'
+   
    
 
 
